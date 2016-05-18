a174df225902d75a686ba0a90849cdba51c5b12b
Arne Köckeritz <arne@komoot.de>
2016-05-18 16:59:21 +0200

    update libs to okhttp 3.2.0 okhttpx-urlconnection 3.2.0

--- MapboxAndroidSDK/src/main/java/com/mapbox/mapboxsdk/tileprovider/tilesource/TileJsonTileLayer.java
+++ MapboxAndroidSDK/src/main/java/com/mapbox/mapboxsdk/tileprovider/tilesource/TileJsonTileLayer.java
@@ -2,14 +2,16 @@ package com.mapbox.mapboxsdk.tileprovider.tilesource;
 
 import android.os.AsyncTask;
 import android.util.Log;
+
 import com.mapbox.mapboxsdk.geometry.BoundingBox;
 import com.mapbox.mapboxsdk.geometry.LatLng;
 import com.mapbox.mapboxsdk.util.NetworkUtils;
 import com.mapbox.mapboxsdk.util.constants.UtilConstants;
-import com.squareup.okhttp.Cache;
+
 import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
+
 import java.io.ByteArrayOutputStream;
 import java.io.File;
 import java.io.IOException;
@@ -18,120 +20,73 @@ import java.net.HttpURLConnection;
 import java.net.URL;
 import java.util.UUID;
 
+import okhttp3.Cache;
+
 /**
  * A type of tile layer that loads tiles from the internet and metadata about itself
  * with the <a href='https://github.com/mapbox/tilejson-spec'>TileJSON</a> standard.
  */
-public class TileJsonTileLayer extends WebSourceTileLayer {
+public class TileJsonTileLayer extends WebSourceTileLayer
+{
+	// Constants
+	// =================================================================================================================================================================================================
 
     private static final String TAG = "TileJsonTileLayer";
+
+	// Instance Vars
+	// =================================================================================================================================================================================================
 
     private JSONObject tileJSON;
     private Cache cache;
 
-    public TileJsonTileLayer(final String pId, final String url, final boolean enableSSL) {
+	// Constructors
+	// =================================================================================================================================================================================================
+
+	/**
+	 *
+	 * @param pId
+	 * @param url
+	 * @param enableSSL
+	 */
+    public TileJsonTileLayer(final String pId, final String url, final boolean enableSSL)
+	{
         super(pId, url, enableSSL);
 
-        File cacheDir =
-                new File(System.getProperty("java.io.tmpdir"), UUID.randomUUID().toString());
-        try {
+        File cacheDir = new File(System.getProperty("java.io.tmpdir"), UUID.randomUUID().toString());
+
+		try
+		{
             cache = NetworkUtils.getCache(cacheDir, 1024);
-        } catch (Exception e) {
+        }
+		catch (Exception e)
+		{
             Log.e(TAG, "Cache creation failed.", e);
         }
 
         String jsonURL = this.getBrandedJSONURL();
-        if (jsonURL != null) {
+
+        if (jsonURL != null)
             fetchBrandedJSONAndInit(jsonURL);
-        }
     }
 
-    private void initWithTileJSON(JSONObject aTileJSON) {
-        this.setTileJSON((aTileJSON != null) ? aTileJSON : new JSONObject());
-        if (aTileJSON != null) {
-            if (this.tileJSON.has("tiles")) {
-                try {
-                    setURL(this.tileJSON.getJSONArray("tiles").getString(0).replace(".png", "{2x}.png"));
-                } catch (JSONException e) {
-                    Log.e(TAG, "Couldn't set tile url", e);
-                }
-            }
-            mMinimumZoomLevel = getJSONFloat(this.tileJSON, "minzoom");
-            mMaximumZoomLevel = getJSONFloat(this.tileJSON, "maxzoom");
-            mName = this.tileJSON.optString("name");
-            mDescription = this.tileJSON.optString("description");
-            mAttribution = this.tileJSON.optString("attribution");
-            mLegend = this.tileJSON.optString("legend");
+	// Public Methods
+	// =================================================================================================================================================================================================
 
-            double[] center = getJSONDoubleArray(this.tileJSON, "center", 3);
-            if (center != null) {
-                mCenter = new LatLng(center[0], center[1], center[2]);
-            }
-            double[] bounds = getJSONDoubleArray(this.tileJSON, "bounds", 4);
-            if (bounds != null) {
-                mBoundingBox = new BoundingBox(bounds[3], bounds[2], bounds[1], bounds[0]);
-            }
-        }
-        if (UtilConstants.DEBUGMODE) {
-            Log.d(TAG, "TileJSON " + this.tileJSON.toString());
-        }
-    }
+    public JSONObject getTileJSON() { return tileJSON; }
 
-    public JSONObject getTileJSON() {
-        return tileJSON;
-    }
+    public void setTileJSON(JSONObject aTileJSON) { this.tileJSON = aTileJSON; }
 
-    public void setTileJSON(JSONObject aTileJSON) {
-        this.tileJSON = aTileJSON;
-    }
+	// Package Protected Methods
+	// =================================================================================================================================================================================================
 
-    private float getJSONFloat(JSONObject JSON, String key) {
-        float defaultValue = 0;
-        if (JSON.has(key)) {
-            try {
-                return (float) JSON.getDouble(key);
-            } catch (JSONException e) {
-                return defaultValue;
-            }
-        }
-        return defaultValue;
-    }
-
-    private double[] getJSONDoubleArray(JSONObject JSON, String key, int length) {
-        double[] defaultValue = null;
-        if (JSON.has(key)) {
-            try {
-                boolean valid = false;
-                double[] result = new double[length];
-                Object value = JSON.get(key);
-                if (value instanceof JSONArray) {
-                    JSONArray array = ((JSONArray) value);
-                    if (array.length() == length) {
-                        for (int i = 0; i < array.length(); i++) {
-                            result[i] = array.getDouble(i);
-                        }
-                        valid = true;
-                    }
-                } else {
-                    String[] array = JSON.getString(key).split(",");
-                    if (array.length == length) {
-                        for (int i = 0; i < array.length; i++) {
-                            result[i] = Double.parseDouble(array[i]);
-                        }
-                        valid = true;
-                    }
-                }
-                if (valid) {
-                    return result;
-                }
-            } catch (JSONException e) {
-                return defaultValue;
-            }
-        }
-        return defaultValue;
-    }
-
-    byte[] readFully(InputStream in) throws IOException {
+	/**
+	 *
+	 * @param in
+	 * @return
+	 * @throws IOException
+	 */
+    byte[] readFully(InputStream in) throws IOException
+	{
         ByteArrayOutputStream out = new ByteArrayOutputStream();
         byte[] buffer = new byte[1024];
         for (int count; (count = in.read(buffer)) != -1;) {
@@ -140,7 +95,132 @@ public class TileJsonTileLayer extends WebSourceTileLayer {
         return out.toByteArray();
     }
 
-    private void fetchBrandedJSONAndInit(String url) {
+	// Private Methods
+	// =================================================================================================================================================================================================
+
+	/**
+	 *
+	 * @param JSON
+	 * @param key
+	 * @return
+	 */
+	private float getJSONFloat(JSONObject JSON, String key)
+	{
+		float defaultValue = 0;
+		if (JSON.has(key))
+		{
+			try
+			{
+				return (float) JSON.getDouble(key);
+			}
+			catch (JSONException e)
+			{
+				return defaultValue;
+			}
+		}
+		return defaultValue;
+	}
+
+	/**
+	 *
+	 * @param JSON
+	 * @param key
+	 * @param length
+	 * @return
+	 */
+	private double[] getJSONDoubleArray(JSONObject JSON, String key, int length)
+	{
+		double[] defaultValue = null;
+		if (JSON.has(key))
+		{
+			try
+			{
+				boolean valid = false;
+				double[] result = new double[length];
+				Object value = JSON.get(key);
+
+				if (value instanceof JSONArray)
+				{
+					JSONArray array = ((JSONArray) value);
+					if (array.length() == length)
+					{
+						for (int i = 0; i < array.length(); i++)
+							result[i] = array.getDouble(i);
+
+						valid = true;
+					}
+				}
+				else
+				{
+					String[] array = JSON.getString(key).split(",");
+
+					if (array.length == length)
+					{
+						for (int i = 0; i < array.length; i++)
+							result[i] = Double.parseDouble(array[i]);
+
+						valid = true;
+					}
+				}
+
+				if (valid)
+					return result;
+			}
+			catch (JSONException e)
+			{
+				return defaultValue;
+			}
+		}
+		return defaultValue;
+	}
+
+	/**
+	 *
+	 * @param aTileJSON
+	 */
+	private void initWithTileJSON(final JSONObject aTileJSON)
+	{
+		this.setTileJSON((aTileJSON != null) ? aTileJSON : new JSONObject());
+
+		if (aTileJSON != null)
+		{
+			if (this.tileJSON.has("tiles"))
+			{
+				try
+				{
+					setURL(this.tileJSON.getJSONArray("tiles").getString(0).replace(".png", "{2x}.png"));
+				}
+				catch (JSONException e)
+				{
+					Log.e(TAG, "Couldn't set tile url", e);
+				}
+			}
+			mMinimumZoomLevel = getJSONFloat(this.tileJSON, "minzoom");
+			mMaximumZoomLevel = getJSONFloat(this.tileJSON, "maxzoom");
+			mName = this.tileJSON.optString("name");
+			mDescription = this.tileJSON.optString("description");
+			mAttribution = this.tileJSON.optString("attribution");
+			mLegend = this.tileJSON.optString("legend");
+
+			double[] center = getJSONDoubleArray(this.tileJSON, "center", 3);
+
+			if (center != null)
+				mCenter = new LatLng(center[0], center[1], center[2]);
+
+			double[] bounds = getJSONDoubleArray(this.tileJSON, "bounds", 4);
+
+			if (bounds != null)
+				mBoundingBox = new BoundingBox(bounds[3], bounds[2], bounds[1], bounds[0]);
+		}
+
+		if (UtilConstants.DEBUGMODE)
+		{
+			Log.d(TAG, "TileJSON " + this.tileJSON.toString());
+		}
+	}
+
+    private void fetchBrandedJSONAndInit(String url)
+	{
         new RetrieveJSONTask() {
             @Override
             protected void onPostExecute(JSONObject jsonObject) {
@@ -149,29 +229,39 @@ public class TileJsonTileLayer extends WebSourceTileLayer {
         } .execute(url);
     }
 
-    protected String getBrandedJSONURL() {
-        return null;
-    }
+    protected String getBrandedJSONURL() { return null; }
 
-    class RetrieveJSONTask extends AsyncTask<String, Void, JSONObject> {
-        protected JSONObject doInBackground(String... urls) {
+	// Inner classes
+	// =================================================================================================================================================================================================
+
+    class RetrieveJSONTask extends AsyncTask<String, Void, JSONObject>
+	{
+        protected JSONObject doInBackground(String... urls)
+		{
             InputStream in = null;
-            try {
+            try
+			{
                 URL url = new URL(urls[0]);
                 HttpURLConnection connection = NetworkUtils.getHttpURLConnection(url, cache);
                 in = connection.getInputStream();
                 byte[] response = readFully(in);
                 String result = new String(response, "UTF-8");
                 return new JSONObject(result);
-            } catch (Exception e) {
+            }
+			catch (Exception e)
+			{
                 e.printStackTrace();
                 return null;
-            } finally {
-                try {
-                    if (in != null) {
+            }
+			finally
+			{
+                try
+				{
+                    if (in != null)
                         in.close();
-                    }
-                } catch (IOException e) {
+                }
+				catch (IOException e)
+				{
                     Log.e(TAG, "Error closing InputStream: " + e.toString());
                 }
             }
--- MapboxAndroidSDK/src/main/java/com/mapbox/mapboxsdk/util/NetworkUtils.java
+++ MapboxAndroidSDK/src/main/java/com/mapbox/mapboxsdk/util/NetworkUtils.java
@@ -8,45 +8,96 @@ package com.mapbox.mapboxsdk.util;
 import android.content.Context;
 import android.net.ConnectivityManager;
 import android.net.NetworkInfo;
-import com.squareup.okhttp.Cache;
-import com.squareup.okhttp.OkHttpClient;
-import com.squareup.okhttp.OkUrlFactory;
+
 import javax.net.ssl.SSLSocketFactory;
 import java.io.File;
 import java.io.IOException;
 import java.net.HttpURLConnection;
 import java.net.URL;
 
-public class NetworkUtils {
-    public static boolean isNetworkAvailable(Context context) {
-        ConnectivityManager connectivityManager =
-                (ConnectivityManager) context.getSystemService(Context.CONNECTIVITY_SERVICE);
+import okhttp3.Cache;
+import okhttp3.OkHttpClient;
+import okhttp3.internal.huc.HttpURLConnectionImpl;
+import okhttp3.internal.huc.HttpsURLConnectionImpl;
+
+/**
+ * @author Arne
+ */
+public class NetworkUtils
+{
+	/**
+	 *
+	 * @param pContext
+	 * @return
+	 */
+    public static boolean isNetworkAvailable(final Context pContext)
+	{
+        ConnectivityManager connectivityManager = (ConnectivityManager) pContext.getSystemService(Context.CONNECTIVITY_SERVICE);
         NetworkInfo activeNetworkInfo = connectivityManager.getActiveNetworkInfo();
         return activeNetworkInfo != null && activeNetworkInfo.isConnected();
     }
 
-    public static HttpURLConnection getHttpURLConnection(final URL url) {
+	/**
+	 *
+	 * @param url
+	 * @return
+	 */
+    public static HttpURLConnection getHttpURLConnection(final URL url)
+	{
         return getHttpURLConnection(url, null, null);
     }
 
-    public static HttpURLConnection getHttpURLConnection(final URL url, final Cache cache) {
+	/**
+	 *
+	 * @param url
+	 * @param cache
+	 * @return
+	 */
+    public static HttpURLConnection getHttpURLConnection(final URL url, final Cache cache)
+	{
         return getHttpURLConnection(url, cache, null);
     }
 
-    public static HttpURLConnection getHttpURLConnection(final URL url, final Cache cache, final SSLSocketFactory sslSocketFactory) {
-        OkHttpClient client = new OkHttpClient();
-        if (cache != null) {
-            client.setCache(cache);
-        }
-        if (sslSocketFactory != null) {
-            client.setSslSocketFactory(sslSocketFactory);
-        }
-        HttpURLConnection connection = new OkUrlFactory(client).open(url);
+	/**
+	 *
+	 * @param url
+	 * @param cache
+	 * @param sslSocketFactory
+	 *
+	 * @return
+	 */
+    public static HttpURLConnection getHttpURLConnection(final URL url, final Cache cache, final SSLSocketFactory sslSocketFactory)
+	{
+		final OkHttpClient.Builder builder = new OkHttpClient.Builder();
+
+		if (cache != null)
+			builder.cache(cache);
+
+        if (sslSocketFactory != null)
+            builder.sslSocketFactory(sslSocketFactory);
+
+		final OkHttpClient client = builder.build();
+
+        HttpURLConnection connection;
+
+		if (url.getProtocol().equals("https"))
+			connection = new HttpsURLConnectionImpl(url, client);
+		else
+			connection = new HttpURLConnectionImpl(url, client);
+
         connection.setRequestProperty("User-Agent", MapboxUtils.getUserAgent());
         return connection;
     }
 
-    public static Cache getCache(final File cacheDir, final int maxSize) throws IOException {
+	/**
+	 *
+	 * @param cacheDir
+	 * @param maxSize
+	 * @return
+	 * @throws IOException
+	 */
+    public static Cache getCache(final File cacheDir, final int maxSize) throws IOException
+	{
         return new Cache(cacheDir, maxSize);
     }
 }
